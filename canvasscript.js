fbdafd1a17af12e78e0c38c99d469d0d90951f1f
Smith <111470120+Smith2067@users.noreply.github.com>
2023-05-17 17:27:01 -0700

    Add files via upload

---
+++ canvasscript.js
@@ -0,0 +1,9 @@
+const ctx = document.getElementById("canvas").getContext("2d");
+ctx.beginPath();
+ctx.lineWidth = '0px';
+ctx.fillStyle = '#ff0000';
+ctx.fillRect(39, 28, 145, 188);
+ctx.fillRect(185, 57, 66, 134);
+ctx.fillStyle = '#ffffff';
+ctx.fillRect(53, 51, 98, 63);
+ctx.stroke();
